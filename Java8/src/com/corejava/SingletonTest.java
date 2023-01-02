8be5b381424757a12383b2e1050676c73edf6366
kr12061985 <krishnakishore.msc@gmail.com>
2023-01-02 18:34:36 +0530

    commit in dev

--- Java8/src/com/corejava/SingletonTest.java
+++ Java8/src/com/corejava/SingletonTest.java
@@ -4,7 +4,7 @@ public class SingletonTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first comment in dev");
+		System.out.println("first comment in devvvvvv");
 		Test test=Test.getiInstace();
 		Test test1=Test.getiInstace();
 		System.out.println(test);
